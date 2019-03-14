afc2caf0b994e3a522d7dc34deb68050e04015b7
ElectricFly <31453510+ElectricFly@users.noreply.github.com>
2019-03-14 21:14:15 +0100

    Removed Device Credentials from Code
    Old credentials are invalid now.

--- LoraSender.ino
+++ LoraSender.ino
@@ -10,17 +10,17 @@
 // first. When copying an EUI from ttnctl output, this means to reverse
 // the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
 // 0x70.
-static const u1_t PROGMEM APPEUI[8]={ 0x9A, 0x8F, 0x01, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 };
+static const u1_t PROGMEM APPEUI[8]={ APPUI_SETME };
 void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
 
 // This should also be in little endian format, see above.
-static const u1_t PROGMEM DEVEUI[8]={ 0x52, 0x0C, 0x5E, 0x72, 0x41, 0xCE, 0xCE, 0x00 };
+static const u1_t PROGMEM DEVEUI[8]={ DEVEUI_SETME };
 void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
 
 // This key should be in big endian format (or, since it is not really a
 // number but a block of memory, endianness does not really apply). In
 // practice, a key taken from ttnctl can be copied as-is.
-static const u1_t PROGMEM APPKEY[16] = { 0x7E, 0x7A, 0x98, 0x60, 0x27, 0x53, 0x9C, 0x7A, 0x6D, 0x57, 0x95, 0x15, 0x34, 0xC2, 0xAA, 0xBD };
+static const u1_t PROGMEM APPKEY[16] = { APPKEY_SETME };
 void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
 
 static uint8_t mydata[] = "Hello, world!";
